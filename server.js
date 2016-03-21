5a45ac2187e1124fec1c2f9bce4b6a84774d7829
Rob Schmidt <Rschmidt2616@conestogac.on.ca>
2016-03-21 12:00:45 -0400

    edited code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-var util= require('util');
+var util = require('util');
 
 var ipaddr = process.env.OPENSHIFT_NODESJS_IP || "127.0.0.1";
 
